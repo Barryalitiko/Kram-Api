f24b9024fc516355e2d76ec672f45793dbf5baa6
Krampus B. <tiobarryx@gmail.com>
2025-01-07 22:21:21 +0100

    Actualizar index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 async function start() {
   try {
     const app = express();
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 4000;
 
     app.use(express.json());
 

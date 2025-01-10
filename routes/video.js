4730a5da1a21462d3444fbfbaac6c2a59686ed59
Krampus B. <tiobarryx@gmail.com>
2025-01-10 16:15:24 +0100

    Actualizar video.js

--- routes/video.js
+++ routes/video.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const { getYouTubeVideoInfo, selectBestVideoFormat } = require("../services/youtubeService");
+const {
+  getYouTubeVideoInfo,
+  selectBestVideoFormat,
+} = require("../services/youtubeService");
+const ytdl = require("ytdl-core");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const { url } = req.query;
 
   try {
+    if (!url) {
+      throw new Error("No se proporcionó una URL");
+    }
+
     // Obtener información del video
     const info = await getYouTubeVideoInfo(url);
 
     // Seleccionar el mejor formato disponible
     const videoFormat = selectBestVideoFormat(info.formats);
 
-    console.log(`Video encontrado: ${info.videoDetails.title} (${url})`);
+    console.log(`Video encontrado: ${info.videoDetails.title}`);
+    console.log(`URL del video: ${url}`);
     console.log(`Formato seleccionado: ${videoFormat.mimeType}`);
 
     res.header("Content-Disposition", 'attachment; filename="video.mp4"');

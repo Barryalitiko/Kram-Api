a5ed935bd0a2ebc6fc0d3d2f6446c141f96c312f
Krampus B. <tiobarryx@gmail.com>
2025-01-11 17:44:24 +0100

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
-const logger = require("./utils/logger"); // Importar el logger personalizado
-const audioRoutes = require("./routes/audio"); // Ruta de audio
-const musicaRoutes = require("./routes/musica"); // Ruta de música
-const videoRoutes = require("./routes/video"); // Ruta de video
-const playdlAudioRoutes = require("./routes/playdlAudio"); // Ruta de audio con Play-DL
+const fs = require("fs"); // Para leer los archivos
+const logger = require("./utils/logger");
+const audioRoutes = require("./routes/audio");
+const musicaRoutes = require("./routes/musica");
+const videoRoutes = require("./routes/video");
+const playdlAudioRoutes = require("./routes/playdlAudio");
 
 async function start() {
   try {
@@ -22,11 +23,31 @@ async function start() {
       res.json({ message: "Bienvenido a la API" });
     });
 
+    // Ruta para ver los archivos en la carpeta public
+    app.get("/public-files", (req, res) => {
+      const publicDir = path.join(__dirname, 'public');
+      fs.readdir(publicDir, (err, files) => {
+        if (err) {
+          return res.status(500).send("No se pudieron leer los archivos.");
+        }
+        res.send(`
+          <html>
+            <body>
+              <h1>Archivos en public:</h1>
+              <ul>
+                ${files.map(file => `<li><a href="/${file}" target="_blank">${file}</a></li>`).join('')}
+              </ul>
+            </body>
+          </html>
+        `);
+      });
+    });
+
     // Usar rutas de audio, música, video y Play-DL
-    app.use("/audio", audioRoutes); // Agregar las rutas de audio
-    app.use("/musica", musicaRoutes); // Agregar las rutas de música
-    app.use("/video", videoRoutes); // Agregar las rutas de video
-    app.use("/playdl-audio", playdlAudioRoutes); // Agregar la nueva ruta de Play-DL
+    app.use("/audio", audioRoutes);
+    app.use("/musica", musicaRoutes);
+    app.use("/video", videoRoutes);
+    app.use("/playdl-audio", playdlAudioRoutes);
 
     app.listen(PORT, () => {
       logger.info(`Servidor corriendo en http://localhost:${PORT}`);

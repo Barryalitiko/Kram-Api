6f3970db56e57161fbc690e6c269e07afba6b5da
Krampus B. <tiobarryx@gmail.com>
2025-01-11 11:38:49 +0100

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const logger = require("./utils/logger"); // Importar el logger personalizado
 const express = require("express");
 const audioRoutes = require("./routes/audio"); // Ruta de audio
+const musicaRoutes = require("./routes/musica"); // Ruta de música
 const videoRoutes = require("./routes/video"); // Ruta de video
+const playdlAudioRoutes = require("./routes/playdlAudio"); // Ruta de audio con Play-DL
 
 async function start() {
   try {
@@ -16,10 +18,11 @@ async function start() {
       res.json({ message: "Bienvenido a la API" });
     });
 
-    // Usar rutas de audio, música y video
+    // Usar rutas de audio, música, video y Play-DL
     app.use("/audio", audioRoutes); // Agregar las rutas de audio
     app.use("/musica", musicaRoutes); // Agregar las rutas de música
     app.use("/video", videoRoutes); // Agregar las rutas de video
+    app.use("/playdl-audio", playdlAudioRoutes); // Agregar la nueva ruta de Play-DL
 
     app.listen(PORT, () => {
       logger.info(`Servidor corriendo en http://localhost:${PORT}`);
